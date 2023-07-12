edf4eefcecf0a631618889110fffab25ac5ff558
Ben-git01 <benbakirbenjamin@orange.fr>
2023-07-12 11:32:15 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+// REGLAGE DU PORT
+const port = 4242;
+
+app.use(cors(
+
+))
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+  });
+
+
+const treasures = 
+[
+    {
+        "id" = 1
+        "is"
+
+    }
+]
