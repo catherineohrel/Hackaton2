c93c677970985ed5df64ebc9f1a07dd6fb83a8b1
catherineohrel <catherine_ohrel@hotmail.com>
2023-07-12 22:37:49 +0200

    update video page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Introduction from "./pages/Introduction";
 import Dialogs from "./pages/Dialogs";
 import Nav from "./components/General/nav";
+import Video from "./pages/Video";
 
 /* SCSS */
 import "./App.scss";
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/intro" element={<Introduction />} />
           <Route path="/dialogs" element={<Dialogs />} />
+          <Route path="/video" element={<Video />} />
         </Routes>
       </section>
     </>
