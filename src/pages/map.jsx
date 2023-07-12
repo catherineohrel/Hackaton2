ca419477a06202329d17d94c9a99e91136bbc0ff
Ben-git01 <benbakirbenjamin@orange.fr>
2023-07-12 21:46:44 +0200

    add menufilter at bottom of map

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -42,47 +42,61 @@ export default function Map() {
 	});
 
 	return (
-		<MapContainer
-			center={[24.551, -77.786]}
-			zoom={7}
-			scrollWheelZoom={true}
-			className="map-container"
-		>
-			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			{treasures.map((treasure) => (
-				<Marker position={treasure.coord} icon={customIcon} key={treasure.id}>
-					<Popup className="popup-container">
-						<img
-							src="src/assets/images/treasure2.png"
-							alt="treasure"
-							className="img-popup"
-						/>
-						<br />
-						Name: {treasure.name} <br />
-						<ul>
-							<li>{treasure.preciousstone}</li>
-							<li>{treasure.goldcoins} gold coins</li>
-							<li>{treasure.silvercoins} silver coins</li>
-						</ul>
-						<br />
-					</Popup>
-				</Marker>
-			))}
-			{ships.map((ship) => (
-				<Marker position={ship.coord} icon={customIcon2} key={ship.id}>
-					<Popup className="popup-container">
-						<img src={ship.url} alt="treasure" className="img-popup" />
-						<br />
-						Name: {ship.name} <br /> Nation: {ship.nation}
-						<br />
-						Treasure: {ship.cargovalue} or <br /> Crew: {ship.members} members
-						<br /> Canons : {ship.canons}
-					</Popup>
-				</Marker>
-			))}
-		</MapContainer>
+		<>
+			<MapContainer
+				center={[24.551, -77.786]}
+				zoom={7}
+				scrollWheelZoom={true}
+				className="map-container"
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				{treasures.map((treasure) => (
+					<Marker position={treasure.coord} icon={customIcon} key={treasure.id}>
+						<Popup className="popup-container">
+							<img
+								src="src/assets/images/treasure2.png"
+								alt="treasure"
+								className="img-popup"
+							/>
+							<br />
+							Name: {treasure.name} <br />
+							<ul>
+								<li>{treasure.preciousstone}</li>
+								<li>{treasure.goldcoins} gold coins</li>
+								<li>{treasure.silvercoins} silver coins</li>
+							</ul>
+							<br />
+						</Popup>
+					</Marker>
+				))}
+				{ships.map((ship) => (
+					<Marker position={ship.coord} icon={customIcon2} key={ship.id}>
+						<Popup className="popup-container">
+							<img src={ship.url} alt="treasure" className="img-popup" />
+							<br />
+							Name: {ship.name} <br /> Nation: {ship.nation}
+							<br />
+							Treasure: {ship.cargovalue} or <br /> Crew: {ship.members} members
+							<br /> Canons : {ship.canons}
+						</Popup>
+					</Marker>
+				))}
+				<div className="menufilter-map">
+					<select name="target" id="target">
+						<option value="">Ships & treasures</option>
+						<option value="">Treasures</option>
+						<option value="">Ships</option>
+					</select>
+					<select name="target" id="target">
+						<option value=""></option>
+						<option value="">Treasures</option>
+						<option value="">Ships</option>
+					</select>
+				</div>
+			</MapContainer>
+		</>
 	);
 }
